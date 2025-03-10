5e27af702cf96fca4fa88f3d2cbad6b7330523a2
Sohailmateen <sohailmateen001@gmail.com>
2025-03-10 11:07:03 +0530

    operations

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -27,3 +27,41 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+console.log("\n");
+
+
+
+// ***************************  Operations *********************
+
+let value = 3
+let nevValue = -value
+console.log(nevValue);
+
+// basic operations 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+console.log("\n");
+
+
+let str1 = "hello"
+let str2 = " sohail"
+
+let str3 = str1 + str2
+console.log((str3));
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((+true));
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
