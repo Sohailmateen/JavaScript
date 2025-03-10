201e8c3385f38571831acb2f44ea3b8dd28c1875
Sohailmateen <sohailmateen001@gmail.com>
2025-03-10 10:50:32 +0530

    conversion of data type

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,29 @@
+let score = "50a";
+
+console.log(typeof(score));
+
+let valNumber = Number(score);
+
+console.log(typeof(valNumber));
+console.log((valNumber));
+
+// 33 => 33
+// "33abc" => NaN
+// true => 1
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sohail" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
