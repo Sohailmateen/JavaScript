95df808a5adba45dbb51b6df8b93786cc359b307
Sohailmateen <sohailmateen001@gmail.com>
2025-03-13 04:49:19 +0530

    while loops

---
+++ 05_loops/while_loops.js
@@ -0,0 +1,22 @@
+// let i = 0
+// while (i <= 10) {
+//     console.log(`value of index is ${i}`);
+//     i +=2;   
+// }
+
+
+
+// let myArr = ["a", "b", "c", "d"]
+// let arr = 0
+// while (arr<myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr++
+// }
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <=10);
